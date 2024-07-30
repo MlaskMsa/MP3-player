dd7f8d7b2a90097dc9681fe6bd6eb87c6ac88e9a
MlaskMsa <ardapolat924@gmail.com>
2024-07-30 14:29:06 +0300

    Added my project

--- MusicPlayer/main.py
+++ MusicPlayer/main.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import Menu
+from tkinter import filedialog  
 
 class Application:
     def __init__(self):
@@ -7,13 +8,17 @@ class Application:
         tk = tkinter.Tk()
         tk.title("Mp3 Player")
         
-        def donothing():
-            print("Hi")
+        #Create a functino to open a file    
+        def open():
+            path = filedialog.askopenfilename()
+            print(path)
+            
         #Created a menu and added a few commands
         menubar = Menu(tk)
         file_menu = Menu(menubar,tearoff=0)
-        file_menu.add_command("Open",command= donothing)
-        file_menu.add_command("Exit",command= donothing)     
+        menubar.add_cascade(menu=file_menu, label="File")
+        file_menu.add_command(label="Open",command=open)
+        tk.config(menu=menubar)
         
         tk.mainloop()
         
